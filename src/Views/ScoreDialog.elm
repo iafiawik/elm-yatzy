91bbdf94c0e1353286191cd3fc0bdcf8414937c0
Sophie <sophie.wikstrom@gmail.com>
2018-10-11 20:05:12 +0200

    Update styling of score dialog

--- src/Views/ScoreDialog.elm
+++ src/Views/ScoreDialog.elm
@@ -7,17 +7,17 @@ import Logic exposing (getAcceptedValues)
 import Models exposing (Box, BoxCategory(..), BoxType(..), Game(..), Model, Msg(..), Player, PlayerAndNumberOfValues, Value)
 
 
-scoreDialogButton : Bool -> Int -> String -> String -> Html Msg
-scoreDialogButton isMarked value buttonText class =
+scoreDialogNumberButton : Bool -> Int -> String -> String -> Html Msg
+scoreDialogNumberButton isMarked value buttonText class =
     button
         [ classList
-            [ ( "input-dialog-number-button button", True )
+            [ ( "score-dialog-number-button button", True )
             , ( "marked", isMarked )
             , ( class, True )
             ]
         , onClick (ValueMarked value)
         ]
-        [ text buttonText ]
+        [ span [] [ text buttonText ] ]
 
 
 scoreDialog : Model -> Box -> Player -> Html Msg
@@ -29,23 +29,20 @@ scoreDialog model box currentPlayer =
         acceptedValuesButtons =
             List.map
                 (\v ->
-                    scoreDialogButton (model.currentValue == v) v (String.fromInt v) "hej"
+                    scoreDialogNumberButton (model.currentValue == v) v (String.fromInt v) ""
                 )
                 acceptedValues
     in
-    div [ class "input-dialog-wrapper dialog-wrapper" ]
-        [ div [ class "input-dialog-background dialog-background  animated fadeIn", onClick HideAddValue ] []
-        , div [ class "input-dialog dialog-content animated jackInTheBox" ]
+    div [ class "dialog-wrapper" ]
+        [ div [ class "dialog-background  animated fadeIn", onClick HideAddValue ] []
+        , div [ class "score-dialog dialog-content animated jackInTheBox" ]
             [ div []
-                [ button [ class "input-dialog-cancel-button button", onClick HideAddValue ] [ text "X" ]
+                [ button [ class "score-dialog-cancel-button button", onClick HideAddValue ] [ text "X" ]
                 , h1 [] [ text box.friendlyName ]
                 , h2 [] [ text currentPlayer.name ]
                 ]
-            , div [ classList [ ( "input-dialog-number-buttons", True ), ( "" ++ box.id_, True ) ] ]
-                (acceptedValuesButtons ++ [ scoreDialogButton (model.currentValue == 0) 0 ":(" "skip-button" ])
-            , div []
-                [ input [ class "input-dialog-input-field", type_ "number", onInput InputValueChange, value (String.fromInt model.currentValue) ] []
-                ]
-            , button [ classList [ ( "input-dialog-submit-button button", True ), ( "enabled animated pulse infinite", model.currentValue >= 0 ) ], disabled (model.currentValue < 0), onClick AddValue ] [ text "Spara" ]
+            , div [ classList [ ( "score-dialog-number-buttons", True ), ( "" ++ box.id_, True ) ] ]
+                (acceptedValuesButtons ++ [ scoreDialogNumberButton (model.currentValue == 0) 0 ":(" "skip-button" ])
+            , button [ classList [ ( "score-dialog-submit-button button", True ), ( "enabled animated pulse infinite", model.currentValue >= 0 ) ], disabled (model.currentValue < 0), onClick AddValue ] [ text "Spara" ]
             ]
         ]
