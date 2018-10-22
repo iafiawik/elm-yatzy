a9a977bbda249f5239d1c9361675d6b8f5bdb8d8
Sophie <sophie.wikstrom@gmail.com>
2018-10-22 20:47:58 +0200

    Update URL

--- src/Views/GameInfo.elm
+++ src/Views/GameInfo.elm
@@ -16,7 +16,7 @@ gameInfo game =
             [ h1 [] [ text "Om du vill:" ]
             , h2 [] [ text "gör så här för att joina detta spel på din mobil" ]
             , ol []
-                [ li [] [ span [ class "game-info-link" ] [ span [] [ text "Gå till" ], a [ href "http://soph.se/yatzy/game" ] [ text "http://soph.se/yatzy/game" ] ] ]
+                [ li [] [ span [ class "game-info-link" ] [ span [] [ text "Gå till" ], a [ href "http://soph.se/yatzy" ] [ text "http://soph.se/yatzy" ] ] ]
                 , li [] [ span [] [ span [] [ text "Skriv i koden" ], span [ class "game-info-code" ] [ text game.code ] ] ]
                 , li [] [ text "Välj ditt namn" ]
                 , li [] [ text "Spela!" ]
