ef6c9e17f76f42bd3a0bf85ef80f60a67ef949e7
Sophie <sophie.wikstrom@gmail.com>
2018-10-24 17:16:29 +0200

    Add TopBar view

---
+++ src/Views/TopBar.elm
@@ -0,0 +1,20 @@
+module Views.TopBar exposing (topBar)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Model.Player exposing (Player)
+import Models exposing (Msg(..))
+
+
+topBar : Bool -> Player -> Html Msg
+topBar isMyTurn currentPlayer =
+    let
+        currentPlayerInfo =
+            if isMyTurn then
+                div [ class "top-bar-not-waiting" ] [ span [] [ text "Det är din tur!" ] ]
+
+            else
+                div [ class "top-bar-waiting" ] [ span [] [ text "Väntar på" ], span [] [ text currentPlayer.user.name ] ]
+    in
+    div [ class "top-bar" ] [ currentPlayerInfo, button [ class "top-bar-info-button", onClick ShowGameInfo ] [ text "info" ] ]
