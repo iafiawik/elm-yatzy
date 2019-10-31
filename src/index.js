e6ef1dcd345e5a7bd1b12ad39b2656d19de7355c
Sophie Wikström <sophie.wikstrom@gmail.com>
2019-10-31 22:04:25 +0100

    Debounce window.blur

--- src/index.js
+++ src/index.js
@@ -131,8 +131,29 @@ if (window.isAdmin) {
   };
 }
 
-window.onblur = function() {
-  console.log("window.onblur ");
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+var myEfficientFn = debounce(function() {
+	onBlur();
+}, 500);
+
+window.addEventListener('blur', myEfficientFn);
+
+function onBlur() {
+  console.log("window.onblur");
 
   if (oldGameAndUserExist()) {
     const gameId = getGameInLocalStorage();
@@ -147,7 +168,7 @@ window.onblur = function() {
     console.log("window.onblur , No old game found, so no need to send blur");
   }
 };
-//
+
 window.onfocus = function() {
   checkLastPlayedGame();
 };
